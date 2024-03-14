e610a8af81486bf73d3ec0731d3431c5c193ec6f
abhinav <abhinav4753.be22@chitkara.edu.in>
2024-03-14 13:28:45 +0530

    krdi ypdate

---
+++ 6.3c/jenkins/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h>Hello welcome to my page</h>
+      <h>hi everyone how are u</h>
+    </div>
+  );
+}
+
+export default App;
